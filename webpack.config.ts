2a1e551fa6546e9d29a9b4e8563f1432a7f36a89
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-12-07 22:44:30 +0800

    add webpack config

---
+++ webpack.config.ts
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  output: {
+    filename: 'yamlify-object.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      name:'yamlifyObject',
+      type: 'umd',
+      export: 'default',
+    },
+    globalObject: 'this',
+    clean: true,
+  },
+};
